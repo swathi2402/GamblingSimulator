39b62e7efaf98b1c94806731b91939a90545c2ae
Swathi <swathi.h@perfios.com>
2021-08-24 09:34:51 +0530

    ADDED: Resign from game if he win or loose 50% of stake

--- src/com/brigdelabz/gamblingsimulator/GamblingSimulator.java
+++ src/com/brigdelabz/gamblingsimulator/GamblingSimulator.java
@@ -3,20 +3,35 @@ package com.brigdelabz.gamblingsimulator;
 public class GamblingSimulator {
 	public static final int START_STAKE = 100;
 	public static final int BET_ON_GAME = 1;
+	public static int stake = 0;
+	public static int win = 0;
+	public static int loose = 0;
 	
 	public static void play() {
-		int stake = 0;
 		if (Math.random() > 0.5) {
 			System.out.println("You won the $1");
 			stake++;
+			win++;
 		}
 		else {
 			System.out.println("You lost the $1");
 			stake--;
+			loose++;
+		}
+	}
+	
+	public static void resignGame() {
+		int halfOfStake = START_STAKE /2;
+		while(stake != halfOfStake) {
+			play();
+		}
+		if (stake == halfOfStake) {
+			System.out.println("Winning count: " + win + " Loosing count: " + loose);;
+			System.out.println("Player resigning the game");
 		}
 	}
 	
 	public static void main(String[] args) {
-		play();
+		resignGame();
 	}
 }
