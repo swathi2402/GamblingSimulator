3c02a8b4463e981d357331b99a6dd195791d234c
Swathi <swathi.h@perfios.com>
2021-08-24 08:29:56 +0530

    ADDED: Class GamblingSimulator to start a stake and bet

--- src/com/brigdelabz/gamblingsimulator/GamblingSimulator.java
+++ src/com/brigdelabz/gamblingsimulator/GamblingSimulator.java
@@ -1,8 +1,12 @@
 package com.brigdelabz.gamblingsimulator;
 
 public class GamblingSimulator {
+	public static final int START_STAKE = 100;
+	public static final int BET_ON_GAME = 1;
 	public static void main(String[] args) {
-		int startStake = 100;
-		int betOnGame = 1;
+		
+		
+		System.out.println("Stake is: " + START_STAKE);
+		System.out.println("Bet price on a day: " + BET_ON_GAME);
 	}
 }
