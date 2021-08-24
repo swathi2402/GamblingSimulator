6fe91cb17ac60dcaf789a4985047fd6f81cef2c6
Swathi <swathi.h@perfios.com>
2021-08-24 09:07:37 +0530

    REFACTORED: Addede

--- src/com/brigdelabz/gamblingsimulator/GamblingSimulator.java
+++ src/com/brigdelabz/gamblingsimulator/GamblingSimulator.java
@@ -4,7 +4,7 @@ public class GamblingSimulator {
 	public static final int START_STAKE = 100;
 	public static final int BET_ON_GAME = 1;
 	
-	public static void main(String[] args) {
+	public static void play() {
 		int stake = 0;
 		if (Math.random() > 0.5) {
 			System.out.println("You won the $1");
@@ -14,5 +14,9 @@ public class GamblingSimulator {
 			System.out.println("You lost the $1");
 			stake--;
 		}
+	}
+	
+	public static void main(String[] args) {
+		play();
 	}
 }
