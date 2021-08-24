e394dc6a2ec3bc768e35cfa9f31189fbd60fac3e
Swathi <swathi.h@perfios.com>
2021-08-24 09:08:12 +0530

    Revert "REFACTORED: Addede"
    This reverts commit 6fe91cb17ac60dcaf789a4985047fd6f81cef2c6.

--- src/com/brigdelabz/gamblingsimulator/GamblingSimulator.java
+++ src/com/brigdelabz/gamblingsimulator/GamblingSimulator.java
@@ -4,7 +4,7 @@ public class GamblingSimulator {
 	public static final int START_STAKE = 100;
 	public static final int BET_ON_GAME = 1;
 	
-	public static void play() {
+	public static void main(String[] args) {
 		int stake = 0;
 		if (Math.random() > 0.5) {
 			System.out.println("You won the $1");
@@ -14,9 +14,5 @@ public class GamblingSimulator {
 			System.out.println("You lost the $1");
 			stake--;
 		}
-	}
-	
-	public static void main(String[] args) {
-		play();
 	}
 }
