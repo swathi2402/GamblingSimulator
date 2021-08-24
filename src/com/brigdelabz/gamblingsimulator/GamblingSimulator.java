0530e7476c24e4f22e51196cb37b89a356f6cffa
Swathi <swathi.h@perfios.com>
2021-08-24 09:04:13 +0530

    REFACTORED: Added logic to keep track of stake

--- src/com/brigdelabz/gamblingsimulator/GamblingSimulator.java
+++ src/com/brigdelabz/gamblingsimulator/GamblingSimulator.java
@@ -4,12 +4,15 @@ public class GamblingSimulator {
 	public static final int START_STAKE = 100;
 	public static final int BET_ON_GAME = 1;
 	
-	public static void main(String[] args) {				
+	public static void main(String[] args) {
+		int stake = 0;
 		if (Math.random() > 0.5) {
 			System.out.println("You won the $1");
+			stake++;
 		}
 		else {
 			System.out.println("You lost the $1");
+			stake--;
 		}
 	}
 }
