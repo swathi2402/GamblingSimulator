9cc29ae0b41cc985fe353ea17ac20c6f5d550108
Swathi <swathi.h@perfios.com>
2021-08-24 21:44:18 +0530

    ADDED: Logic to calculate total amount at the end of 20 days

--- src/com/brigdelabz/gamblingsimulator/GamblingSimulator.java
+++ src/com/brigdelabz/gamblingsimulator/GamblingSimulator.java
@@ -7,45 +7,46 @@ public class GamblingSimulator {
 	public static int stake = 0;
 	public static int win = 0;
 	public static int loose = 0;
-	
+
 	public static void play() {
 		if (Math.random() > 0.5) {
-			System.out.println("You won the $1");
 			stake++;
-		}
-		else {
-			System.out.println("You lost the $1");
+		} else {
 			stake--;
 		}
 	}
-	
+
 	public static int resignGame() {
 		win = (int) Math.round(START_STAKE + (START_STAKE * 0.5));
 		loose = (int) Math.round(START_STAKE * 0.5);
-		System.out.println("Winning amount: " + win);
-		System.out.println("Loosing amount: " + loose);
-		
+
 		boolean resign = true;
 		stake = START_STAKE;
-		while(resign == true) {
+		while (resign == true) {
 			play();
-			if(stake == win) {
+			if (stake == win) {
 				resign = false;
 			}
-			if(stake == loose) {
+			if (stake == loose) {
 				resign = false;
 			}
 		}
 		return stake;
 	}
-	
+
 	public static void main(String[] args) {
-		int stackeValue = resignGame();
-		if(stackeValue == win) {
-			System.out.println("Player retires game as he wins enough");
+		int totalAmount = 0;
+		for (int index = 1; index <= DAYS_OF_PLAYING; index++) {
+			int stackeValue = resignGame();
+			totalAmount += stackeValue;
+			System.out.print("Day " + index + ": ");
+			if (stackeValue == win) {
+				System.out.println("Player retires game as he wins enough for the day");
+			}
+			if (stackeValue == loose) {
+				System.out.println("Player retires game as he looses enough for the day");
+			}
 		}
-		if(stackeValue == loose) {
-			System.out.println("Player retires game as he looses enough");
-		}
+		System.out.println("Total amount after " + DAYS_OF_PLAYING + " is: " + totalAmount);
 	}
 }
