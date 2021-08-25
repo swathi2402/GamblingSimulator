fbf3fafadc81930b6f54befcc1cb949e7621bf88
Swathi <swathi.h@perfios.com>
2021-08-25 17:05:43 +0530

    ADDED: loop to know lucky and unlucky month while betting

--- src/com/brigdelabz/gamblingsimulator/GamblingSimulator.java
+++ src/com/brigdelabz/gamblingsimulator/GamblingSimulator.java
@@ -3,7 +3,8 @@ package com.brigdelabz.gamblingsimulator;
 public class GamblingSimulator {
 	public static final int START_STAKE = 100;
 	public static final int BET_ON_GAME = 1;
-	public static final int DAYS_OF_PLAYING = 20;
+	public static final int DAYS_OF_PLAYING = 30;
+	public static final int MONTH_PER_YEAR = 12;
 	public static int stake = 0;
 	public static int win = 0;
 	public static int loose = 0;
@@ -38,21 +39,34 @@ public class GamblingSimulator {
 		int totalAmount = 0;
 		int winCount = 0;
 		int looseCount = 0;
-		for (int index = 1; index <= DAYS_OF_PLAYING; index++) {
-			int stackeValue = resignGame();
-			totalAmount += stackeValue;
-			System.out.print("Day " + index + ": ");
-			if (stackeValue == win) {
-				winCount++;
-				System.out.println("Player retires game as he wins enough for the day");
+		int luckyMonth = 0;
+		int unluckyMonth = (int) (DAYS_OF_PLAYING * (START_STAKE + (START_STAKE * 0.5)));
+		for (int months = 1; months <= MONTH_PER_YEAR; months++) {
+			winCount = 0;
+			looseCount = 0;
+			for (int index = 1; index <= DAYS_OF_PLAYING; index++) {
+				int stackeValue = resignGame();
+				totalAmount += stackeValue;
+				if (stackeValue == win) {
+					winCount++;
+				}
+				if (stackeValue == loose) {
+					looseCount++;
+				}
 			}
-			if (stackeValue == loose) {
-				looseCount++;
-				System.out.println("Player retires game as he looses enough for the day");
+			System.out.println("Month " + months + ": ");
+			System.out.println("Total number of days won the bet: " + winCount);
+			System.out.println("Total number of days lost the bet: " + looseCount);
+			System.out.println("Total amount at the end of month is: " + totalAmount);
+			
+			if(luckyMonth < totalAmount) {
+				luckyMonth = totalAmount;
+			}
+			if(unluckyMonth > totalAmount) {
+				unluckyMonth = totalAmount;
 			}
 		}
-		System.out.println("Total number of days won the bet: " + winCount);
-		System.out.println("Total number of days lost the bet: " + looseCount);
-		System.out.println("Total amount after " + DAYS_OF_PLAYING + " days of playing is: " + totalAmount);
+		System.out.println("Total amount in a lucky month: " + luckyMonth);
+		System.out.println("Total amount in an unlucky month: " + unluckyMonth);
 	}
 }
